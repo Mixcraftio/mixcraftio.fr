3998dc9c54a6719ac0dafbf4a56220fdb1c3ddc2
Mixcraftio <dimitri.faj@gmail.com>
2024-05-09 11:59:56 +0200

    Update css and remove jquery

--- static/global.js
+++ static/global.js
@@ -1,16 +1,30 @@
 
 function replaceComponents() {
     document.querySelectorAll('component').forEach(component => {
-        $(component).load("/components/"+ component.id + "/" + component.id+".html")
-        $.getScript("/components/"+ component.id + "/" + component.id+".js")
+        fetch("/components/"+ component.id + "/" + component.id+".html")
+        .then(res=>{
+            return res.text()
+        }).then(html=>{
+            component.innerHTML += html;
+        })
+
+        js_script = document.createElement('script')
+        js_script.src = "/components/"+ component.id + "/" + component.id+".js"
+        component.appendChild(js_script)
     })
 }
 
 
-$.ajax({ type: "GET",
-    url: "/static/imports.html",
-    success : function(text){
-        $('head').append(text)
-        replaceComponents()
-    }
+js_script = document.createElement('script')
+js_script.type = "module"
+js_script.src = "https://esm.run/@material/web/all.js"
+document.querySelector("head").appendChild(js_script)
+
+fetch("/static/imports.html")
+.then(res=>{
+    return res.text()
+}).then(html=>{
+    document.querySelector("head").innerHTML += html
+}).then(()=>{
+    replaceComponents()
 })
