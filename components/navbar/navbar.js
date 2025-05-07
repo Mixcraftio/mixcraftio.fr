23e4126fd5093677e7e4b3144436f8787e60a4a6
Mixcraftio <dimitri.faj@gmail.com>
2025-05-08 00:59:59 +0200

    Change theme + fix colors css + fix active navbar

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,8 +1,14 @@
-var urlpath = window.location.pathname
+var urlpath = window.location.pathname;
+var urlSegment = urlpath.split("/")[1]; // Get the first path segment
+
 var activetabset = setInterval(() => {
-var activetab = document.querySelector("[href='/"+urlpath.split("/")[1]+"']")
-    if (activetab) {
-        activetab.classList.add("active")
-        clearInterval(activetabset)
-    }
+    // Try to find a link whose href starts with the same segment
+    var links = document.querySelectorAll("#navbar nav a");
+    links.forEach(link => {
+        var linkSegment = link.getAttribute("href").replace(/^\/+|\/+$/g, "").split("/")[0];
+        if (linkSegment === urlSegment) {
+            link.classList.add("active");
+            clearInterval(activetabset);
+        }
+    });
 }, 100);
